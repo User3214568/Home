dc6b9c64a41d5778d39864d24e4fceb8a0b9a886
User3214568 <77564132+User3214568@users.noreply.github.com>
2021-09-27 16:08:44 +0100

    before using mix to compile js css

--- resources/views/parts/admin/old/index.blade.php
+++ resources/views/parts/admin/old/index.blade.php
@@ -5,6 +5,12 @@
 </div>
 
 <div class="row mt-4">
+    @if (sizeof($formations) == 0)
+        <div class="row text-center">
+            Vous n'avez pas des années universitaire antérieurs à afficher. Verifier que vous avez
+            effectuer les délibration du fin d'année dans la section Etudiants > Délibrations
+        </div>
+    @endif
     @include("parts.admin.old.header")
 </div>
 
